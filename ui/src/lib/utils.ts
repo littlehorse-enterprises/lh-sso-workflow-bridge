70d3f0e4f1d5f115512501226d74d42c86ddf4a9
Bryson G. <bryson.glembin@gmail.com>
2025-01-21 14:16:18 -0800

    refactor: use reduce

--- ui/src/lib/utils.ts
+++ ui/src/lib/utils.ts
@@ -10,21 +10,21 @@ export function getRoles(obj: any) {
   const paths = splitPaths.map((path) => path.split(".").slice(1));
 
   const roles = paths.flatMap((pathArray) => {
-    let current = [obj];
-    pathArray.forEach((key) => {
-      if (key === "*") {
-        current = current.flatMap((item) => {
-          if (typeof item === "object" && item !== null) {
-            return Object.values(item);
-          }
-          return [];
-        });
-      } else {
-        current = current.flatMap((item) => item[key]).filter(Boolean);
-      }
-      if (current.length === 0) return;
-    });
-    return current;
+    return pathArray.reduce(
+      (current, key) => {
+        if (key === "*") {
+          return current.flatMap((item) => {
+            if (typeof item === "object" && item !== null) {
+              return Object.values(item);
+            }
+            return [];
+          });
+        } else {
+          return current.flatMap((item) => item[key]).filter(Boolean);
+        }
+      },
+      [obj],
+    );
   });
 
   return roles;
