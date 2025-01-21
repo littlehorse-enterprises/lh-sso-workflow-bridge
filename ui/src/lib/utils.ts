2cf9a34bfb8b1302dec3983eea19f7db22c7d506
Bryson G. <bryson.glembin@gmail.com>
2025-01-21 12:49:38 -0800

    feat: env var authorities config

--- ui/src/lib/utils.ts
+++ ui/src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getRoles(obj: any) {
+  const splitPaths = process.env.AUTHORITIES?.split(",") ?? [];
+  const paths = splitPaths.map((path) => path.split(".").slice(1));
+
+  const roles = paths.flatMap((pathArray) => {
+    let current = [obj];
+    pathArray.forEach((key) => {
+      if (key === "*") {
+        current = current.flatMap((item) => {
+          if (typeof item === "object" && item !== null) {
+            return Object.values(item);
+          }
+          return [];
+        });
+      } else {
+        current = current.flatMap((item) => item[key]).filter(Boolean);
+      }
+      if (current.length === 0) return;
+    });
+    return current;
+  });
+
+  return roles;
+}
